e4d1ee487f324e55540ddc709b3def941b27860b
Michael McKenna <michael.mckenna@outlook.com>
2018-12-15 17:29:25 +1300

    Solution for day 09 👍

---
+++ AOC2018.Tests/Day09Test.cs
@@ -0,0 +1,29 @@
+using AOC2018.Solver;
+using Xunit;
+
+namespace AOC2018.Tests
+{
+    public class Day09Test
+    {
+
+        [Theory]
+        [InlineData("9 players; last marble is worth 25 points", "32")]
+        [InlineData("10 players; last marble is worth 1618 points", "8317")]
+        [InlineData("13 players; last marble is worth 7999 points", "146373")]
+        [InlineData("17 players; last marble is worth 1104 points", "2764")]
+        [InlineData("21 players; last marble is worth 6111 points", "54718")]
+        [InlineData("30 players; last marble is worth 5807 points", "37305")]
+        public void TestDay09PartASolver(string input, string expectedResult)
+        {
+            Assert.Equal(expectedResult, new Day09Solver().SolveA(input));
+        }
+
+        //[Theory]
+        //[InlineData("2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2", "66")]
+        //public void TestDay09PartBSolver(string input, string expectedResult)
+        //{
+        //    Assert.Equal(expectedResult, new Day09Solver().SolveB(input));
+        //}
+
+    }
+}
---
+++ AOC2018/Solver/Day09Solver.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AOC2018.Solver
+{
+    /// <summary>
+    /// Solution for the ninth day advent problem
+    /// </summary>
+    [AdventDay(day: 9)]
+    public class Day09Solver : ISolver
+    {
+        public string ProblemTitle => "Day 9: Marble Mania";
+        
+        /// <summary>
+        /// Calculate the winning score
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public string SolveA(string input)
+        {
+            var numbers = new Regex(@"\d+")
+                .Matches(input);
+
+            int numberOfPlayers = int.Parse(numbers[0].Value);
+            int maxMarble = int.Parse(numbers[1].Value);
+
+            return $"{CalculateWinningScore(numberOfPlayers, maxMarble)}";
+        }
+        
+        private Int64 CalculateWinningScore(int numberOfPlayers, int maxMarble)
+        {
+            var scores = new Int64[numberOfPlayers];
+            var marbles = new LinkedList<int>(new[] { 0 });
+            var currentMarble = marbles.First;
+
+            for (int i = 1; i <= maxMarble; i++)
+            {
+                if (i % 23 == 0 && i != 0)
+                {
+                    var currentPlayer = i % numberOfPlayers;
+                    var extraPointMarble = currentMarble;
+                    for (int j = 0; j < 8; j++)
+                    {
+                        if (extraPointMarble.Previous == null)
+                        {
+                            extraPointMarble = marbles.Last;
+                        }
+                        else
+                        {
+                            extraPointMarble = extraPointMarble.Previous;
+                        }
+                    }
+
+                    scores[currentPlayer] = scores[currentPlayer] + i + extraPointMarble.Value;
+                    currentMarble = extraPointMarble.Next;
+                    marbles.Remove(extraPointMarble);
+                }
+                else
+                {
+                    marbles.AddAfter(currentMarble, i);
+                    currentMarble = currentMarble.Next;
+                }
+
+                //If last space wrap around
+                if (currentMarble.Next == null)
+                {
+                    currentMarble = marbles.First;
+                }
+                else
+                {
+                    currentMarble = currentMarble.Next;
+                }
+            }
+
+            return scores.Max();
+        }
+
+        /// <summary>
+        /// Calculate the winning score if 100x more marbles
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public string SolveB(string input)
+        {
+            var numbers = new Regex(@"\d+")
+                .Matches(input);
+
+            int numberOfPlayers = int.Parse(numbers[0].Value);
+            int maxMarble = int.Parse(numbers[1].Value) * 100;
+
+            return $"{CalculateWinningScore(numberOfPlayers, maxMarble)}";
+        }
+    }
+}
